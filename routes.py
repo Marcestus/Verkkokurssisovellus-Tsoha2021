9dce0f74f88ab22c8d5746d8256ac637854ea4d1
Marcestus <55045740+Marcestus@users.noreply.github.com>
2021-04-24 16:11:43 +0300

    Add basic login, register and logout features

--- routes.py
+++ routes.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect
+import users
 
 @app.route("/")
 def index():
@@ -8,3 +9,30 @@ def index():
 @app.route("/statistics")
 def statistics():
     return render_template("statistics.html")
+
+@app.route("/login", methods=["post"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    if users.login(username,password):
+        return redirect("/")
+    else:
+        return render_template("error.html",message="Väärä tunnus tai salasana")
+
+@app.route("/register", methods=["get","post"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        usertype = request.form["usertype"]
+        if users.register(username,password,usertype):
+            return redirect("/")
+        else:
+            return render_template("error.html",message="Rekisteröinti ei onnistunut")
+
+@app.route("/logout")
+def logout():
+    users.logout()
+    return redirect("/")
---
+++ users.py
@@ -0,0 +1,37 @@
+from db import db
+from os import urandom
+from flask import session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+def login(username,password):
+    sql = "SELECT password, id, usertype FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone()
+    if user == None:
+        return False
+    else:
+        if check_password_hash(user[0],password):
+            session["user_id"] = user[1]
+            session["username"] = username
+            session["usertype"] = user[2]
+            return True
+        else:
+            return False
+
+def logout():
+    del session["user_id"]
+    del session["username"]
+    del session["usertype"]
+
+def register(username,password,usertype):
+    hash_value = generate_password_hash(password)
+    try:
+        sql = "INSERT INTO users (username,password,usertype) VALUES (:username,:password,:usertype)"
+        db.session.execute(sql, {"username":username,"password":hash_value,"usertype":usertype})
+        db.session.commit()
+    except:
+        return False
+    return login(username,password)
+
+def user_id():
+    return session.get("user_id",0)
