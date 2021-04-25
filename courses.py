8c43ef8f83fa35b46cb56aed52fb7e9e4ec4fa40
Marcestus <55045740+Marcestus@users.noreply.github.com>
2021-04-25 11:22:14 +0300

    Add features: signup to courses, go to course page

--- courses.py
+++ courses.py
@@ -3,20 +3,45 @@ import users
 
 def owned_courses():
     owner_id = users.user_id()
-    sql = "SELECT coursename FROM courses WHERE owner_id=:owner_id"
+    sql = "SELECT id, coursename FROM courses WHERE owner_id=:owner_id"
     result = db.session.execute(sql, {"owner_id":owner_id})
     return result.fetchall()
 
 def users_courses():
     user_id = users.user_id()
-    sql = "SELECT C.coursename FROM participants P, courses C WHERE P.student_id=:user_id AND P.course_id=C.id"
+    sql = "SELECT C.id, C.coursename FROM participants P, courses C WHERE P.student_id=:user_id AND P.course_id=C.id"
     result = db.session.execute(sql, {"user_id":user_id})
     return result.fetchall()
 
-def open_courses():
-    sql = "SELECT coursename FROM courses"
+def all_courses():
+    user_id = users.user_id
+    sql = "SELECT id, coursename FROM courses"
     result = db.session.execute(sql)
     return result.fetchall()
+
+def open_courses():
+    # Voiko tämän suorittaa yhdellä kyselyllä?
+
+    # Haetaan erikseen kaikki kurssit ja kurssit,
+    # joille opiskelija on ilmoittautunut
+    other_courses = all_courses()
+    signedup_courses = users_courses()
+
+    # Käydään läpi kaikki kurssit ja
+    # valitaan avoimiin kursseihin vain ne,
+    # joihin opiskelija ei ole ilmoittautunut
+    open_courses = []
+    for course in other_courses:
+        if not course in signedup_courses:
+            open_courses.append(course)
+    
+    return open_courses
+
+
+def get_course(id):
+    sql = "SELECT * FROM courses WHERE id=:id"
+    result = db.session.execute(sql, {"id":id})
+    return result.fetchone()
 
 def create_new(coursename):
     owner_id = users.user_id()
@@ -28,4 +53,13 @@ def create_new(coursename):
     except:
         return False
     return True
-    
+
+def signup_to_course(course_id):
+    user_id = users.user_id()
+    try:
+        sql = "INSERT INTO participants (student_id, course_id) VALUES (:user_id,:course_id)"
+        db.session.execute(sql, {"user_id":user_id,"course_id":course_id})
+        db.session.commit()
+    except:
+        return False
+    return True
--- routes.py
+++ routes.py
@@ -5,6 +5,8 @@ import users, courses
 @app.route("/")
 def index():
 
+    # Tarviiko tässä huomioida kirjautumista muuten kuin index näyttää session-tietojen mukaan eri näkymän?
+
     if users.get_usertype() == 0:
         users_courses = courses.users_courses()
         open_courses = courses.open_courses()
@@ -17,15 +19,43 @@ def index():
 
 @app.route("/newcourse", methods=["post"])
 def newcourse():
+
+    # Tääkin pitää varmaan suojata niin, että vain (kirjautuneet) opet pääsee tähän sivulle
+
     coursename = request.form["coursename"]
     if courses.create_new(coursename):
         return redirect("/")
     else:
-        return render_template("error.html", message="Kurssin lisääminen ei onnistunut")
+        return render_template("error.html",message="Kurssin lisääminen ei onnistunut")
+
+@app.route("/course/<int:id>")
+def course(id):
+
+    # Tänne pitää nyt ainakin laittaa se,
+    # että jos on opettajaroolissa niin ei pääse kuin omalle sivulleen
+    # Eli jos usertype on ope, owner_id pitää olla user_id ennen kun näkee sivun
+
+    # Ja tähän myös, että jos ei oo kirjautunut, niin ei näe sivua
+
+    course = courses.get_course(id)
+    return render_template("course.html",course=course)
+
+@app.route("/signup/<int:id>")
+def signup(id):
+    if courses.signup_to_course(id):
+        return redirect("/")
+    else:
+        return render_template("error.html",message="Kurssille ilmoittautuminen ei onnistunut")
 
 @app.route("/statistics")
 def statistics():
+
+    # Täällä pitää varmistaa, että jokainen näkee vain omat tietonsa
+    # Eikä pääse kirjautumatta sivulle
+
     return render_template("statistics.html")
+
+
 
 @app.route("/login", methods=["post"])
 def login():
