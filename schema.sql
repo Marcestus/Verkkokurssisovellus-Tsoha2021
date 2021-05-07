5222e9348482edccd8ee1d7f0bf94547bdc96edd
Marcestus <55045740+Marcestus@users.noreply.github.com>
2021-05-07 20:46:07 +0300

    Remove ordervalues datatable, it seems irrelevant

--- schema.sql
+++ schema.sql
@@ -20,30 +20,13 @@ CREATE TABLE Participants (
     student_id INTEGER REFERENCES users,
     course_id INTEGER REFERENCES courses
 );
-CREATE TABLE Ordervalues (
-    id SERIAL PRIMARY KEY,
-    ordervalue INTEGER
-);
 CREATE TABLE Coursematerials (
     id SERIAL PRIMARY KEY,
     course_id INTEGER REFERENCES courses,
-    material_order INTEGER REFERENCES ordervalues,
+    material_order INTEGER,
     material_title TEXT,
     material_content TEXT
 );
 
 INSERT INTO Usertypes (usertype_id,usertype_name) VALUES (1, 'teacher');
 INSERT INTO Usertypes (usertype_id,usertype_name) VALUES (2, 'student');
-INSERT INTO Ordervalues (ordervalue) VALUES (1);
-INSERT INTO Ordervalues (ordervalue) VALUES (2);
-INSERT INTO Ordervalues (ordervalue) VALUES (3);
-INSERT INTO Ordervalues (ordervalue) VALUES (4);
-INSERT INTO Ordervalues (ordervalue) VALUES (5);
-INSERT INTO Ordervalues (ordervalue) VALUES (6);
-INSERT INTO Ordervalues (ordervalue) VALUES (7);
-INSERT INTO Ordervalues (ordervalue) VALUES (8);
-INSERT INTO Ordervalues (ordervalue) VALUES (9);
-INSERT INTO Ordervalues (ordervalue) VALUES (10);
-
-SELECT * FROM Usertypes;
-SELECT * FROM Ordervalues;
